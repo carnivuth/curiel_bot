1bdc3d0ee7165f6744320f0718b5423a000d18b0
carnivuth <matti200042@gmail.com>
2024-10-15 16:08:37 +0200

    added preso tutto command

--- src/spesa/preso.js
+++ src/spesa/preso.js
@@ -1,25 +1,28 @@
 percistance=require("../common/percistance")
 module.exports=function preso(bot){
-    bot.onText(/\/preso (.+)/, (msg, match) => {
-        console.log(msg);
-        var resp;
-        const chatId = msg.chat.id;
-      
-        //objectsTaken = match[1].split(" ");
-        objectsTaken = match[1];
+  bot.onText(/\/preso (.+)/, (msg, match) => {
+    console.log(msg);
+    var resp;
+    const chatId = msg.chat.id;
 
-      
-        mancanze = percistance.loadFromJson("mancanze-"+chatId+".json");
-        console.log(mancanze);
-        mancanze = mancanze.filter(function (item) {
-          return !objectsTaken.includes(item.mancanza);
-        });
-      
-        percistance.saveToJson("mancanze-"+chatId+".json", mancanze);
-      
-        resp = "mancanze aggiornate!";
-        console.log(resp);
-      
-        bot.sendMessage(chatId, resp);
+    //objectsTaken = match[1].split(" ");
+    objectsTaken = match[1];
+    if (objectsTaken == 'tutto'){
+      percistance.saveToJson("mancanze-"+chatId+".json", []);
+    }else{
+
+      mancanze = percistance.loadFromJson("mancanze-"+chatId+".json");
+      console.log(mancanze);
+      mancanze = mancanze.filter(function (item) {
+        return !objectsTaken.includes(item.mancanza);
       });
+
+      percistance.saveToJson("mancanze-"+chatId+".json", mancanze);
+    }
+
+    resp = "mancanze aggiornate!";
+    console.log(resp);
+
+    bot.sendMessage(chatId, resp);
+  });
 }
